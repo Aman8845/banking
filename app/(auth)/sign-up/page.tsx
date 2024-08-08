652d1ab7000e2aecb1e1d53c4216bb671d141802
Aman Verma <amanv9952@gmail.com>
2024-08-08 22:37:58 +0530

    progress commit

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+// import AuthForm from '@/components/AuthForm'
+
+const SignUp = async () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      AuthForm
+    </section>
+  )
+}
+
+export default SignUp
